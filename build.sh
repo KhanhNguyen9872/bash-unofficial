f64ba3cd5a7f0117ac3e399fc8f0e38b85312b2c
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2025-03-04 19:11:15 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -14,12 +14,14 @@ make -j$(nproc || 2)
 if [ -f './bash' ]; then
     printf "\nBuild process completed!\n"
     printf "\nDo you want install to system? [Y/n]: "
-    read -p choose
+    read choose
     if [[ "$choose" == "Y" ]] || [[ "$choose" == "y" ]]; then
         bash_path = "$(which bash)"
         mv "$bash_path" "$bash_path.old"
-        cp ./bash "$bash_path" || printf "\nFailed when install to system! You can using this command in root user: `cp ./bash \"$bash_path\"`\n"
+        cp ./bash "$bash_path" || { printf "\nFailed when install to system! You can using this command in root user: `cp ./bash \"$bash_path\"`\n"; exit 0 }
         chmod 777 "$bash_path"
+        printf "\nInstalled! Please restart shell to make it work!\n"
+        exit 0
     fi
 else
     printf "\nBuild process failed!\n"
